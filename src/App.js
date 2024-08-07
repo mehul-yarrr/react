e6756415cff4fb98b038ef29da649f25a587cfb2
unknown <tanwarsinghmehul@gmail.com>
2024-08-07 22:49:17 +0530

    gg

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import about from './components/about';
 function App() {
   return (
     <>
-      <Navbar title="NewText" AboutUs="AboutUs" />
+      <Navbar title="Jenni ki website!" AboutUs="AboutUs" />
       <div className="container"></div>
       <TextForm heading="Heading of Text Area" />
       
